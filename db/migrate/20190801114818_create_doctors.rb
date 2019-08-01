6bd9c98e0deabcbb19fd3559d152af81465b771d
Alex <alex@roadsign.fr>
2019-08-01 19:53:32 +0200

    exos

---
+++ db/migrate/20190801114818_create_doctors.rb
@@ -0,0 +1,16 @@
+class CreateDoctors < ActiveRecord::Migration[5.2]
+  def change
+    create_table :doctors do |t|
+      t.string :first_name
+      t.string :string
+      t.string :last_name
+      t.string :string
+      #t.string :speciality
+      #t.string :string
+      t.string :zip_code
+      t.string :string
+      t.belongs_to :city, index: true
+      t.timestamps
+    end
+  end
+end
